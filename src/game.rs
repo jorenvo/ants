d68b6b701adc0cb1b3076612fbae0f16cb949c28
Joren Van Onder <joren@jvo.sh>
2019-07-03 20:52:24 -0700

    merge pheromones

--- src/game.rs
+++ src/game.rs
@@ -82,6 +82,44 @@ impl Game {
         }
     }
 
+    fn merge_and_clear_pheromones(
+        &mut self,
+        pos: &PositionComponent,
+        extra_strength: u8,
+    ) -> IntensityComponent {
+        let mut intensity = IntensityComponent {
+            strength: extra_strength,
+        };
+        let mut pheromones_to_delete = vec![];
+
+        if let Some(entities) = self.entity_store.get_entities_at(pos) {
+            let pheromones: Vec<&EntityIndex> = entities
+                .iter()
+                .filter(|id| {
+                    self.entity_store.entity_types.get(id).unwrap() == &EntityType::Pheromone
+                })
+                .collect();
+
+            if !pheromones.is_empty() {
+                let merged_strength: u8 = pheromones
+                    .iter()
+                    .map(|p| self.entity_store.intensities.get(p).unwrap().strength)
+                    .sum();
+                intensity.strength += merged_strength;
+
+                pheromones_to_delete = pheromones.iter().map(|p| **p).collect();
+            }
+        }
+
+        for ph in pheromones_to_delete {
+            self.entity_store.remove_position(&ph);
+            self.entity_store.intensities.remove(&ph);
+            self.entity_store.pheromones.remove(&ph);
+        }
+
+        intensity
+    }
+
     fn release_pheromones(&mut self, ant_id: &EntityIndex) {
         if let Some(releasing_pheromone_comp) =
             self.entity_store.releasing_pheromones.get_mut(ant_id)
@@ -92,13 +130,13 @@ impl Game {
             } else {
                 match releasing_pheromone_comp.ph_type {
                     PheromoneType::Food => {
-                        let ph_id = self.entity_store.create_entity(&EntityType::Pheromone);
-                        self.entity_store
-                            .intensities
-                            .insert(ph_id, IntensityComponent { strength: 8 });
-
                         let ant_pos = self.entity_store.get_position(ant_id).unwrap().clone();
+                        let intensity = self.merge_and_clear_pheromones(&ant_pos, 4);
+
+                        // TODO create pheromone_types component BTreeMap in entity store
+                        let ph_id = self.entity_store.create_entity(&EntityType::Pheromone);
                         self.entity_store.update_position(&ph_id, &ant_pos);
+                        self.entity_store.intensities.insert(ph_id, intensity);
                     }
                 }
             }
@@ -153,6 +191,23 @@ impl Game {
 
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // internal state
+        writeln!(f, "COMPONENTS")?;
+        writeln!(f, "----------")?;
+        writeln!(f, "positions: {:#?}", self.entity_store.positions)?;
+        writeln!(
+            f,
+            "positions_lookup: {:#?}",
+            self.entity_store.positions_lookup
+        )?;
+        writeln!(
+            f,
+            "releasing_pheromones: {:#?}",
+            self.entity_store.releasing_pheromones
+        )?;
+        writeln!(f, "intensities: {:#?}", self.entity_store.intensities)?;
+        writeln!(f, "----------")?;
+
         let integer_width = self.width.round() as u64;
         let integer_height = self.height.round() as u64;
         let separator =
@@ -164,7 +219,10 @@ impl fmt::Display for Game {
             for col in 0..integer_width {
                 let mut cell_color = "white";
                 let mut cell_value: String = "■■".to_string();;
-                let pos = PositionComponent { x: col as f64, y: row as f64};
+                let pos = PositionComponent {
+                    x: col as f64,
+                    y: row as f64,
+                };
 
                 if let Some(ids) = self.entity_store.get_entities_at(&pos) {
                     for id in ids {
